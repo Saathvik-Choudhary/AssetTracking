1059b43d7ce633f90f40fab9c59237ec5610819b
ishika <jainishika336@gmail.com>
2024-04-17 11:43:03 +0530

    Test cases.

--- src/test/java/com/example/AssetTracking/persistence/AssetRepositoryTests.java
+++ src/test/java/com/example/AssetTracking/persistence/AssetRepositoryTests.java
@@ -4,8 +4,10 @@ import com.example.AssetTracking.Domain.Asset;
 import com.example.AssetTracking.Persistence.AssetRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,11 +16,13 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class AssetRepositoryTests {
 
     @Autowired
@@ -29,24 +33,37 @@ public class AssetRepositoryTests {
         assertNotNull(subject);
 
     }
-
     @Mock
     private AssetRepository mockAssetRepository;
 
+    private Asset asset1;
+    private Asset asset2;
+
     @BeforeEach
     public void setUp(){
+        asset1 = new Asset();
+        asset1.setTitle("Asset 1");
+        asset1.setCost(BigDecimal.valueOf(1000));
+        asset1.setDepreciationRate(BigDecimal.valueOf(5));
+        asset1.setPurchaseDate(LocalDate.now().minusYears(1));
+
+
+        asset2 = new Asset();
+        asset2.setTitle("Asset 2");
+        asset2.setCost(BigDecimal.valueOf(2000));
+        asset2.setDepreciationRate(BigDecimal.valueOf(2));
+        asset2.setPurchaseDate(LocalDate.now().minusYears(4));
+
+        when(mockAssetRepository.findAll()).thenReturn(Arrays.asList(asset1,asset2));
+        mockAssetRepository.saveAll(Arrays.asList(asset1,asset2));
         MockitoAnnotations.openMocks(this);
     }
 
     @Test
     public void testSize(){
-        List<Asset> assets = Arrays.asList(
-                new Asset("Asset 1", BigDecimal.valueOf(1000),BigDecimal.valueOf(5), LocalDate.now()),
-                new Asset("Asset 2", BigDecimal.valueOf(2000),BigDecimal.valueOf(10), LocalDate.now())
-        );
-        when(mockAssetRepository.findAll()).thenReturn(assets);
-
-        Long size = mockAssetRepository.size();
-        assertEquals(2L, size);
+        long size = mockAssetRepository.size();
+        assertEquals(2,size);
     }
+
+
 }
